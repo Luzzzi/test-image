20832dd5d5791225244478d2e9dd80bba42a8b30
Lucie Le Frapper <lucielefrapper@macbook-pro-de-lucie.home>
2023-05-19 15:51:17 +0200

    test 2

--- project-test-image/app/page.tsx
+++ project-test-image/app/page.tsx
@@ -7,10 +7,10 @@ export default function Home() {
     <main className="w-full">
       <div className="flex flex-row">
         <div className="w-[416px] h-[740px] bg-blue-400">
-          <Image src={Frago} alt="" className="object-cover" />
+          <Image src={Frago} alt="" fill={true} className="object-cover" />
         </div>
         <div className="w-[416px] h-[740px] bg-red-500">
-          <Image src={Eyck} alt="" className="w-full h-auto object-cover" />
+          <Image src={Eyck} alt="" fill={true} className="object-cover" />
         </div>
       </div>
     </main>
